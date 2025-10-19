2df512307fc12282a1684fcdb06ca34ddfa86200
jacky <jackylvm@foxmail.com>
2025-10-19 21:16:43 +0800

    第三课内容提交

--- Assets/Basics/Lesson03/Scripts/FunctionLibrary.cs
+++ Assets/Basics/Lesson03/Scripts/FunctionLibrary.cs
@@ -22,12 +22,14 @@ namespace Basics.Lesson03
     {
         Wave,
         MultiWave,
-        Ripple
+        Ripple,
+        Sphere,
+        Torus
     }
 
     public static class FunctionLibrary
     {
-        private static Function[] functions = new Function[] { Wave, MultiWave, Ripple };
+        private static Function[] functions = new Function[] { Wave, MultiWave, Ripple, Sphere, Torus };
 
         public static Function GetFunction(FunctionName name)
         {
@@ -78,6 +80,61 @@ namespace Basics.Lesson03
             p.y = Sin(PI * (4f * d - t));
             p.y /= 1f + 10f * d;
             p.z = v;
+            return p;
+        }
+
+        private static Vector3 Sphere(float u, float v, float t)
+        {
+            /*
+            var r = Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = r * Sin(PI * u);
+            // p.y = v;
+            p.y = Sin(0.5f * PI * v);
+            p.z = r * Cos(PI * u);
+            return p;
+            */
+            // var r = 0.5f + 0.5f * Sin(PI * t);
+            // var r = 0.9f + 0.1f * Sin(8f * PI * u);
+            // var r = 0.9f + 0.1f * Sin(8f * PI * v);
+            var r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+            var s = r * Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(0.5f * PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+
+        private static Vector3 Torus(float u, float v, float t)
+        {
+            /*
+            var r = 1f;
+            var s = 0.5f + r * Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(0.5f * PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+            */
+            /*
+            var r = 1f;
+            var s = 0.5f + r * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+            */
+            // const float r1 = 0.75f;
+            // const float r2 = 0.25f;
+            var r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+            var r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+            var s = r1 + r2 * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r2 * Sin(PI * v);
+            p.z = s * Cos(PI * u);
             return p;
         }
     }
